2ce002a75df6c43827e791e30b3e8aac9aaa96be
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-11-24 00:01:12 -0300

    criando dbConfig

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,1 +1,17 @@
-a
+import { MongoClient } from 'mongodb';
+
+export default async function conectarAoBanco(CONNECTIONSTRING) {
+    let mongoClient;
+
+    try {
+        mongoClient = new MongoClient(CONNECTIONSTRING);
+        console.log('Conectando ao cluster do banco de dados...');
+        await mongoClient.connect();
+        console.log('Conectado ao MongoDB Atlas com sucesso!');
+
+        return mongoClient;
+    } catch (erro) {
+        console.error('Falha na conexão com o banco!', erro);
+        process.exit();
+    }
+}
