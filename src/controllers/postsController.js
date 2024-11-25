e6e8dbf22116370d399d1dc1c66ad720b125fe48
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-11-25 01:07:00 -0300

    implementacao do google ia

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
 import {getTodosPosts, criarPost, atualizarPost} from "../models/postsModel.js";
+import gerarDescricaoComGemini from "../services/geminiService.js";
 import fs from "fs";
 
 export async function listarPosts(req, res) {
@@ -41,14 +42,21 @@ export async function uploadImagem(req, res) {
 export async function atualizarNovoPost(req, res) {
     const id = req.params.id;
     const urlImagem = `http://localhost:300${id}.png`;
-    const post = {
-        imgUrl: urlImagem,
-        descricao: req.body.descricao,
-        alt: req.body.alt
-    };
+
+    
 
     try {
+        
+        const imageBuffer = fs.readFileSync(`uploads/${id}.png`);
+        const descricao = await gerarDescricaoComGemini(imageBuffer);
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        };
         const postCriado = await atualizarPost(id, post );
+
+        
         res.status(200).json(postCriado);  
     } catch(erro) {
         console.error(erro.message);
