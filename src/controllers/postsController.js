80b53b5aa679af4c9617cb4aa798e3addfec02f7
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-11-24 23:48:49 -0300

    teste

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,38 +1,38 @@
-import {getTodosPosts, criarPost} from "../models/postsModels.js";
+import {getTodosPosts, criarPost} from "../models/postsModel.js";
+import fs from "fs";
 
-
-export async function listarPosts (req, res){
-    const posts =  await getTodosPosts();
+export async function listarPosts(req, res) {
+    // Chama a função para buscar os posts
+    const posts = await getTodosPosts();
+    // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON
     res.status(200).json(posts);
 }
 
-export async function postarNovoPost(req, res){
+export async function postarNovoPost(req, res) {
     const novoPost = req.body;
-    try{
+    try {
         const postCriado = await criarPost(novoPost);
-        res.status(201).json(postCriado);
-
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
     }
-    catch(erro){
-        console.log(erro.message);
-        res.status(500).json({message: "Erro ao criar post"});
-    }
-    
 }
 
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
 
-export async function uploadImagem(req, res){
-    const novoPost = req.body;
-    try{
+    try {
         const postCriado = await criarPost(novoPost);
-        res.status(201).json(postCriado);
-
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
     }
-    catch(erro){
-        console.log(erro.message);
-        res.status(500).json({message: "Erro ao criar post"});
-    }
-    
 }
-
-
