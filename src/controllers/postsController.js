6a3e5016ac73e27df411c9b1fa9bb86748f470c0
henriqueCostaSI <henrique.mcosta@hotmail.com>
2024-11-25 00:45:39 -0300

    teste

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,5 @@
-import {getTodosPosts, criarPost} from "../models/postsModel.js";
+import { atualizarPost } from "../models/postsModel.js";
+import {getTodosPosts, criarPost, atualizarPost} from "../models/postsModel.js";
 import fs from "fs";
 
 export async function listarPosts(req, res) {
@@ -30,6 +31,25 @@ export async function uploadImagem(req, res) {
         const postCriado = await criarPost(novoPost);
         const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
         fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
+
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:300${id}.png`;
+    const post = {
+        imgUrl: urlImagem,
+        descricao: req.body.descricao,
+        alt: req.body.alt
+    };
+
+    try {
+        const postCriado = await atualizarPost(id, post );
         res.status(200).json(postCriado);  
     } catch(erro) {
         console.error(erro.message);
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,6 +1,7 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js"
 // Conecta ao banco de dados utilizando a string de conexão fornecida como variável de ambiente
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+const conexao = await conectarAoBanco(process.env.CONNECTIONSTRING);
 
 // Função assíncrona para buscar todos os posts do banco de dados
 export async function getTodosPosts() {
@@ -16,4 +17,11 @@ export async function criarPost(novoPost) {
     const db = conexao.db("imersao-instabytes");
     const colecao = db.collection("posts");
     return colecao.insertOne(novoPost)
+}
+
+export async function atualizarPost(id, novoPost){
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    const objID = ObjectId.createFromHexString(id);
+    return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost})
 }
